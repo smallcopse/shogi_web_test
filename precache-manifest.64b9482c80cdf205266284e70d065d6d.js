e31be5e38729896a005888fc80f195e797c35357
Daisuke Kobayashi <smallcopse@users.noreply.github.com>
2020-09-02 23:00:56 +0900

    Updates

---
+++ precache-manifest.64b9482c80cdf205266284e70d065d6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3807d4b1cbc8fd2a8940c0831a95c2f9",
+    "url": "/shogi_web_test/index.html"
+  },
+  {
+    "revision": "57dc497af7e1f22501bb",
+    "url": "/shogi_web_test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "223ee29f2cebf2ccac84",
+    "url": "/shogi_web_test/static/js/2.7e7d0e48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shogi_web_test/static/js/2.7e7d0e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57dc497af7e1f22501bb",
+    "url": "/shogi_web_test/static/js/main.a47cc246.chunk.js"
+  },
+  {
+    "revision": "8247d8fd03fcb27b6608",
+    "url": "/shogi_web_test/static/js/runtime-main.37435011.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shogi_web_test/static/media/logo.5d5d9eef.svg"
+  }
+]);
